474172192981687dd77e38350dc50b96f3a4175c
Lucas Gabriel <lucas2015.gabrill@hotmail.com>
2023-02-01 13:30:05 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 conexao = "sqlite:///desafio.sqlite"
 SQLALCHEMY_DATABASE_URI = conexao
 ts = '1'
-hash = 'c47c5e5f1196a209dd589562daf786c0'
-apikey = '91a435121ad71a1975634243a12941a1'
+hash = ''
+apikey = ''
 
